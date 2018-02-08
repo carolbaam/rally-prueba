d94cd114a5cb594452c5a9e8a96dd6d46305baa4
carolbaam <carolinabaam@gmail.com>
2018-02-07 19:38:48 -0600

    login

--- js/app.js
+++ js/app.js
@@ -8,3 +8,33 @@ var config = {
     messagingSenderId: "43278050775"
   };
   firebase.initializeApp(config);
+
+  $("#buttonGoogle").click(function(){
+    authGoogle();
+  })
+  
+  function authGoogle (){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    authentication(provider);
+  }
+  
+  function authentication(provider){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(result);
+      // ...
+    }).catch(function(error) {
+      
+      var errorCode = error.code;
+      console.log(error);
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
