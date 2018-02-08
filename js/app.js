3a393aa93d82e29995ccdc9bc6cc470faea9b517
carolbaam <carolinabaam@gmail.com>
2018-02-07 19:17:51 -0600

    probando

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+var config = {
+    apiKey: "AIzaSyB-PNTVuADClpdqZcSCb_hjSspoWdIUMpo",
+    authDomain: "rally-prueba.firebaseapp.com",
+    databaseURL: "https://rally-prueba.firebaseio.com",
+    projectId: "rally-prueba",
+    storageBucket: "",
+    messagingSenderId: "43278050775"
+  };
+  firebase.initializeApp(config);
